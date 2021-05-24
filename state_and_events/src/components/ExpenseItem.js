89782dac5ad77cb18c0b3e0bef14f28fa957946b
oimoralest <oimoralest@gmail.com>
2021-05-24 18:39:11 -0500

    Added clickHandler event

--- state_and_events/src/components/ExpenseItem.js
+++ state_and_events/src/components/ExpenseItem.js
@@ -3,6 +3,9 @@ import ExpenseDate from './ExpenseDate'
 
 
 const ExpenseItem = (props) => {
+	const clickHandler = () => {
+		console.log('You clicked me')
+	}
 	return (
 		<div className="expense-item">
 			<ExpenseDate date={props.date}/>
@@ -10,6 +13,7 @@ const ExpenseItem = (props) => {
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">{props.amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change title</button>
 		</div>
 	);
 }
