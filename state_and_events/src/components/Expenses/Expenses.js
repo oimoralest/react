cd4a9d8a15326d78cc98b5fc294778aa9c58bd24
oimoralest <oimoralest@gmail.com>
2021-05-26 17:52:55 -0500

    Added ExpensesFilter component

--- state_and_events/src/components/Expenses/Expenses.js
+++ state_and_events/src/components/Expenses/Expenses.js
@@ -1,9 +1,21 @@
 import '../../styles/Expenses/Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from '../UI/ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+	const [filteredYear, setFilteredYear] = useState('2020');
+
+	const onFilterChangeHandler = (year) => {
+		setFilteredYear(year);
+	};
+
 	return (
 		<div className="expenses">
+			<ExpensesFilter
+				onFilterChange={onFilterChangeHandler}
+				defaultValue={filteredYear}
+			/>
 			{props.items.map((item) => {
 				return (
 					<ExpenseItem
