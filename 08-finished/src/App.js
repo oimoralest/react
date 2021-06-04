f25737de5f41a50efcdaf8fa2385575e8cd03977
oimoralest <oimoralest@gmail.com>
2021-06-04 19:01:24 +0000

    Solving Just one root element with React Fragments

--- 08-finished/src/App.js
+++ 08-finished/src/App.js
@@ -2,26 +2,25 @@ import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
-import Wrapper from './components/helpers/Wrapper'
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+	const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
+	const addUserHandler = (uName, uAge) => {
+		setUsersList((prevUsersList) => {
+			return [
+				...prevUsersList,
+				{ name: uName, age: uAge, id: Math.random().toString() },
+			];
+		});
+	};
 
-  return (
-    <Wrapper>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </Wrapper>
-  );
+	return (
+		<>
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
+		</>
+	);
 }
 
 export default App;
